f8c20998465c36f083503e898c75391620f45fdb
jacobziemba-dev <jacobziemba9@gmail.com>
2026-01-19 20:07:59 -0500

    all animations working

--- Player/Scripts/state_walk.gd
+++ Player/Scripts/state_walk.gd
@@ -1,13 +1,13 @@
-class_name State_Idle extends State
+class_name State_Walk extends State
 
+@export var move_speed : float = 100.0
 
-@onready var walk : State = $"../Walk"
-
+@onready var idle : State = $"../Idle"
 
 
 # what happens when the player enters this state?
 func Enter() -> void:
-	player.UpdateAnimation("idle")
+	player.UpdateAnimation("walk")
 	pass
 
 
@@ -18,9 +18,14 @@ func Exit() -> void:
 
 # what happens during the _process update in this state?
 func Process(_delta: float) -> State:
-	if player.direction != Vector2.ZERO:
-		return walk
-	player.velocity = Vector2.ZERO
+	if player.direction == Vector2.ZERO:
+		return idle
+	
+	player.velocity = player.direction * move_speed
+	
+
+	if player.SetDirection():
+		player.UpdateAnimation("walk")
 	return null
 
 
